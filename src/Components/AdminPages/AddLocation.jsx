94ca3c318ee5bc01c484267f92b792eb14cd9a46
Ilkay Samet Ozturk <ilk.samett06@gmail.com>
2023-05-28 13:29:28 +0300

    add location component is added.

---
+++ src/Components/AdminPages/AddLocation.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Map } from "../SubComponents/Map";
+
+export const AddLocation = () => {
+  return (
+    <section className="py-5 mt-5">
+      <div className="container py-4 py-xl-5">
+        <div className="row gy-4 gy-md-0">
+          <div className="col-md-3 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
+            <div
+              style={{
+                maxWidth: "380px",
+                border: "2px solid",
+                padding: "10px",
+              }}
+            >
+              <form style={{ textAlign: "center", maxWidth: "360px" }}>
+                <p className="fs-3">Konum Ekle</p>
+                <input
+                  class="form-control"
+                  type="text"
+                  style={{ marginBottom: "12px" }}
+                  placeholder="Konut İsmi"
+                />
+                <input
+                  class="form-control"
+                  type="text"
+                  style={{ marginBottom: "12px" }}
+                  placeholder="Lokasyon İsmi"
+                />
+                <div class="input-group" style={{ marginBottom: "12px" }}>
+                  <input
+                    class="form-control w-100 mb-3"
+                    type="text"
+                    placeholder="Enlem, Boylam"
+                  />
+                </div>
+                <input class="btn btn-primary" type="submit" value={"Kaydet"} />
+              </form>
+            </div>
+          </div>
+          <div className="col-md-9">
+            <div>
+              <Map></Map>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- src/Components/Routes.jsx
+++ src/Components/Routes.jsx
@@ -8,10 +8,11 @@ import { ForgottenPassword } from "./MainPages/ForgottenPassword";
 import { Signup } from "./MainPages/Signup";
 import { NotFoundPage } from "./MainPages/NotFoundPage";
 import { UserMap } from "./UserPages/UserMap";
-import { LocationDetect } from "./ProfessionalPages/LocationDetect"
-import { FilterLocation } from "./UserPages/FilterLocation"
-import { ChangesAccept } from "./AdminPages/ChangesAccept"
-import { ProfessionalChange } from "./AdminPages/ProfessionalControl"
+import { LocationDetect } from "./ProfessionalPages/LocationDetect";
+import { FilterLocation } from "./UserPages/FilterLocation";
+import { ChangesAccept } from "./AdminPages/ChangesAccept";
+import { ProfessionalChange } from "./AdminPages/ProfessionalControl";
+import { AddLocation } from "./AdminPages/AddLocation";
 
 const Paths = () => {
   return (
@@ -26,11 +27,25 @@ const Paths = () => {
       />
       <Route path="/signup" element={<Signup></Signup>} />
       <Route path="*" element={<NotFoundPage></NotFoundPage>} />
+      {/* Users processes*/}
       <Route path="/usermap" element={<UserMap></UserMap>}></Route>
-      <Route path="/userFilterLocation" element={<FilterLocation></FilterLocation>}></Route>
-      <Route path="/professionalMap" element={<LocationDetect></LocationDetect>}></Route>
-      <Route path="/changesAccept" element={<ChangesAccept></ChangesAccept>}></Route>
-      <Route path="/professionalControl" element={<ProfessionalChange></ProfessionalChange>}></Route>
+      <Route
+        path="/userFilterLocation"
+        element={<FilterLocation></FilterLocation>}
+      ></Route>
+      <Route
+        path="/professionalMap"
+        element={<LocationDetect></LocationDetect>}
+      ></Route>
+      <Route
+        path="/changesAccept"
+        element={<ChangesAccept></ChangesAccept>}
+      ></Route>
+      <Route
+        path="/professionalControl"
+        element={<ProfessionalChange></ProfessionalChange>}
+      ></Route>
+      <Route path="/addLocation" element={<AddLocation></AddLocation>}></Route>
     </Routes>
   );
 };
